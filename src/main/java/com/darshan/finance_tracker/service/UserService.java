3ef5457adcc00ef0421f9e4011edad0401325c07
V <2476292@cognizant.com>
2026-08-08 22:07:09 +0530

    Implemented user registration with password encryption

--- src/main/java/com/darshan/finance_tracker/service/UserService.java
+++ src/main/java/com/darshan/finance_tracker/service/UserService.java
@@ -1,6 +1,7 @@
 package com.darshan.finance_tracker.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.darshan.finance_tracker.dto.RegisterRequest;
@@ -13,12 +14,21 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public String register(RegisterRequest request) {
+		if(userRepository
+				.findByEmail(request.getEmail())
+				.isPresent()) {
+				return "Email already exists";
+				}
+		
 		User user=new User();
 		
 		user.setName(request.getName());
 		user.setEmail(request.getEmail());
-		user.setPassword(request.getPassword());
+		user.setPassword(passwordEncoder.encode(request.getPassword()));
 		
 		userRepository.save(user);
 		
